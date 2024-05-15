954b985966e57d1ea68efad133667e2fc52b0512
Kevin Aguilar <44550448+kjaguilar@users.noreply.github.com>
2024-05-15 19:19:28 +0200

    GPIO Driver

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,7 +2,7 @@
  * Application entry point.
  */
 
-#include "mcu.h"
+#include "gpio.h"
 #include "rcc.h"
 #include "FreeRTOSConfig.h"
 #include "FreeRTOSTasks.h"
@@ -33,6 +33,9 @@ int main(void)
   // Update the 'SystemCoreClock' variable required by FreeRTOS
   SystemCoreClockUpdate();
 
+  // Initialize the required GPIOs
+  gpio_init();
+
   // Create the startup task
   startup();
 
@@ -50,6 +53,13 @@ int main(void)
  */
 static void startup_task(void *param)
 {
+  for (;;)
+  {
+    // USER LED test
+    gpio_toggle_pin(USER_LED_PORT, USER_LED_PIN);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+
   // Delete the startup task
   vTaskDelete(NULL);
 }
@@ -61,3 +71,33 @@ static void startup(void)
 {
   configASSERT(pdPASS == xTaskCreate(startup_task, "Startup Task", STARTUP_TASK_STACK_SIZE, NULL, STARTUP_TASK_PRIORITY, NULL));
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
